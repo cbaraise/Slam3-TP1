11c7e11676ade867dee4c627ab6421c692119cc2
Clément Baraise <clement.baraise@gmail.com>
2022-09-21 15:37:48 +0200

    modife commentaire

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                 
                 <?php
                 
-                // Code de vérification 
+                // Code de vérification du login mdp
                 ?>
             </form>
         </div>
